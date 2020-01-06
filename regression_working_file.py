93893018a5f4c245970ce59520471ae1f36cc48d
alexandrujeman <alexandrujeman.ja@gmail.com>
2020-01-06 12:31:40 +0200

    Save/Load trained model with pickle

--- regression_working_file.py
+++ regression_working_file.py
@@ -23,16 +23,23 @@ y = np.array(data[predict])
 # Split data and leave 10% for tests
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
-# Apply linear regression
+# Create linear regression model
 linear = linear_model.LinearRegression()
 
 linear.fit(x_train, y_train)
-acc = linear.score(x_test, y_test)
 
-# Print result
-print(acc)
+# Save/Load trained model on file
+with open("predictionmodel.pickle", "wb") as f:
+    pickle.dump(linear, f)
 
-# Output predictions on test data after model was trained
+pickle_in = open("predictionmodel.pickle", "rb")
+linear = pickle.load(pickle_in)
+
+# Print accuracy result
+accuracy = linear.score(x_test, y_test)
+print(accuracy)
+
+# Output predictions
 predictions = linear.predict(x_test)
 
 for x in range(len(predictions)):
