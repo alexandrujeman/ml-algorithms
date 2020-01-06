a67353d7c7cc7baea06d624df62daf6199f50847
alexandrujeman <alexandrujeman.ja@gmail.com>
2020-01-06 10:35:39 +0200

    Import dependencies, import data set .csv file

---
+++ regression_working_file.py
@@ -0,0 +1,10 @@
+import tensorflow
+import keras
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
+
+data = pd.read_csv("student-mat.csv", sep=";")
+
